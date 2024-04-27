54ebc862b1b3c08051e5a378cbaf9665afd2a5c4
elisah99 <156483036+elisah99@users.noreply.github.com>
2024-04-26 23:20:28 -0400

    Add files via upload

---
+++ MYDESCRIPTION.R
@@ -0,0 +1,11 @@
+Package: RecipeAnalyzer
+Type: Package
+Title: Analyze and Scale Recipes
+Version: 1.0
+Date: 2024-04-25
+Author: Elisa Hardie
+Maintainer: ehardie@usf.edu
+Description: Provides functions to analyze nutritional content, scale recipes, and generate shopping lists.
+License: GNU General Public License
+Imports:
+  Depends: R (>= 4.0)
